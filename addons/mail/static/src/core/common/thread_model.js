6617d6f8ba5cf96057d6cd47a9adba4a1784cc8b
Rahul Prajapati <rapr@odoo.com>
2023-08-16 12:22:51 +0530

    [IMP] mail, {crm,im}_livechat: add /bot command for livechat channels
    PURPOSE:
    
    Allow operators to start chatbot conversations from the list of available
    chatbots with visitor.
    
    SPECIFICATIONS:
    
    - add a /bot command for operators in livechat channels.
    - ENTER: open the list of existing chatbots for selection
    - ENTER: select a chatbot and start chatbot conversation for visitor
    
    task-3373161

--- addons/mail/static/src/core/common/thread_model.js
+++ addons/mail/static/src/core/common/thread_model.js
@@ -592,7 +592,7 @@ export class Thread extends Record {
             "discuss.channel",
             command.methodName,
             [[this.id]],
-            { body }
+            { body, ...command.params }
         );
     }
 
