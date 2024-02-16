a9dd34746402650a509dc499d7b86cfd018b536f
Louis Wicket (wil) <wil@odoo.com>
2024-02-16 10:19:11 +0100

    [IMP] test_assetsbundle: adapt tests to the split between xml and js

--- odoo/addons/test_assetsbundle/tests/test_assetsbundle.py
+++ odoo/addons/test_assetsbundle/tests/test_assetsbundle.py
@@ -592,7 +592,7 @@ class TestJavascriptAssetsBundle(FileTouchable):
 
         bundle = self.env['ir.qweb']._get_asset_bundle('test_assetsbundle.bundle4')
         links = bundle.get_links()
-        self.assertEqual(len(links), 6)
+        self.assertEqual(len(links), 7)
 
         self.assertEqual(str(html.strip()), (f"""<!DOCTYPE html>
 <html>
@@ -604,6 +604,7 @@ class TestJavascriptAssetsBundle(FileTouchable):
         <script type="text/javascript" src="http://test.external.link/javascript1.js"></script>
         <script type="text/javascript" src="http://test.external.link/javascript2.js"></script>
         <script type="text/javascript" src="{links[5]}" onerror="__odooAssetError=1"></script>
+        <script type="text/javascript" src="{links[6]}" onerror="__odooAssetError=1"></script>
     </head>
     <body>
     </body>
@@ -621,6 +622,7 @@ class TestJavascriptAssetsBundle(FileTouchable):
         <script type="text/javascript" src="http://test.external.link/javascript1.js"></script>
         <script type="text/javascript" src="http://test.external.link/javascript2.js"></script>
         <script type="text/javascript" src="/web/assets/debug/test_assetsbundle.bundle4.js" onerror="__odooAssetError=1"></script>
+        <script type="text/javascript" src="/web/assets/debug/test_assetsbundle.bundle4.xml.js" onerror="__odooAssetError=1"></script>
     </head>
     <body>
     </body>
@@ -955,7 +957,7 @@ class TestAssetsManifest(AddonManifestPatched):
             'path': 'http://external.link/external.js',
         })
         bundle = self.env['ir.qweb']._get_asset_bundle('test_assetsbundle.manifest1')
-        scripts = [link for link in bundle.get_links() if link.endswith('js')]
+        scripts = [link for link in bundle.get_links() if link.endswith('js') and not link.endswith('.xml.js')]
         self.assertEqual(len(scripts), 2)
         self.assertEqual(scripts[0], 'http://external.link/external.js')
         attach = bundle.js()
@@ -1334,7 +1336,7 @@ class TestAssetsManifest(AddonManifestPatched):
             }
         }
         bundle = self.env['ir.qweb']._get_asset_bundle('test_assetsbundle.manifest4')
-        scripts = [link for link in bundle.get_links() if link.endswith('js')]
+        scripts = [link for link in bundle.get_links() if link.endswith('js') and not link.endswith('.xml.js')]
         self.assertEqual(len(scripts), 2)
         self.assertEqual(scripts[0], 'http://external.link/external.js')
         attach = bundle.js()
