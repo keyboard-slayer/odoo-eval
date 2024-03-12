d5eb010a56ab2cadbe51496838208f6dc4dd784a
Hesham <hsal@odoo.com>
2024-03-12 12:41:00 +0200

    [IMP] l10n_nl: Added tags needed for PL and BS tags reports
    Added tags needed for the PL and BS reports that use account tags instead of account reports.
    
    Nobody works in the Netherlands in the national standard CoA.
    In the Dutch case, we need an alternative balance sheet and Profit and Loss, built on account tags rather than on account codes.
    These reports needed additional tags to be added.
    
    task-3794536

--- addons/l10n_nl/models/account_chart_template.py
+++ addons/l10n_nl/models/account_chart_template.py
@@ -9,5 +9,9 @@ class AccountChartTemplate(models.AbstractModel):
     def _post_load_data(self, template_code, company, template_data):
         super()._post_load_data(template_code, company, template_data)
         if template_code == 'nl':
-            company.account_journal_suspense_account_id.tag_ids += self.env.ref('l10n_nl.account_tag_25')
-            company.get_unaffected_earnings_account().tag_ids += self.env.ref('l10n_nl.account_tag_12')
+            cash_tag = self.env.ref('l10n_nl.account_tag_cash')
+            company.account_journal_suspense_account_id.tag_ids += cash_tag
+            company.account_journal_payment_debit_account_id.tag_ids += cash_tag
+            company.account_journal_payment_credit_account_id.tag_ids += cash_tag
+            company.transfer_account_id.tag_ids += cash_tag
+            company.get_unaffected_earnings_account().tag_ids += self.env.ref('l10n_nl.account_tag_undist_profit')
