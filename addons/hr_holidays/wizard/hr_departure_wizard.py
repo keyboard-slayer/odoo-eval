921471a50a34b51b57618b955f0249df827a4fd9
Mélanie <mepe@odoo.com>
2024-06-27 16:15:25 +0200

    [IMP] hr_holidays : employee departure
    If a leave/allocation starts before the departure date and ends after
    the departure date; this leave/allocation will be cut in two
    leaves/allocations.
    [leave (start, stop) -- >
        leaves (start, departure date) (departure date +1 day, stop)]
    
    When an employee will be archived, his approved/refused
    leaves/allocation after the departure date will be automatically
    cancelled. His other leaves/allocations after the departure date
    will be deleted.
    
    task-3946565

--- addons/hr_holidays/wizard/hr_departure_wizard.py
+++ addons/hr_holidays/wizard/hr_departure_wizard.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime, timedelta
@@ -14,17 +13,35 @@ class HrDepartureWizard(models.TransientModel):
         employee_leaves = self.env['hr.leave'].search([
             ('employee_id', '=', self.employee_id.id),
             ('date_to', '>', self.departure_date),
-            ('state', '!=', 'refuse')
         ])
-        to_delete = self.env['hr.leave']
-        to_cancel = self.env['hr.leave']
-        for leave in employee_leaves:
-            if leave.state == 'validate':
-                to_cancel |= leave
-            else:
-                to_delete |= leave
-        to_delete.unlink()
-        to_cancel._force_cancel(_('The employee no longer works in the company'), notify_responsibles=False)
+
+        employee_leaves_departure_date = employee_leaves.filtered(lambda leave: leave.date_from.date() <= self.departure_date)
+        leave_to_modify = self.env['hr.leave']
+        values_to_create = []
+        for leave in employee_leaves_departure_date:
+            leave_date_to = leave.request_date_to
+            leave_to_modify += leave
+            values_to_create.append({
+                'request_date_to': leave_date_to,
+                'state': 'cancel',
+                'request_date_from': self.departure_date + timedelta(days=1),
+                'holiday_status_id': leave.holiday_status_id.id,
+                'employee_id': leave.employee_id.id,
+            })
+            leave.with_context(leave_skip_state_check=True).message_post(
+                body=_('End date has been updated because the employee will leave the company on %(departure_date)s.',
+                    departure_date=self.departure_date),
+                subtype_xmlid='mail.mt_comment'
+            )
+        leave_to_modify.with_context(leave_skip_state_check=True).write({'request_date_to': self.departure_date})
+        self.env['hr.leave'].with_context(leave_skip_state_check=True).create(values_to_create)
+        leaves = employee_leaves - employee_leaves_departure_date
+        leave_to_cancel = leaves.filtered(lambda leave: leave.state in ['validate', 'refuse'])
+        leave_to_delete = leaves - leave_to_cancel
+        cancel_text = _('The employee will leave the company on %(departure_date)s.',
+            departure_date=self.departure_date)
+        leave_to_cancel._force_cancel(cancel_text, notify_responsibles=False)
+        leave_to_delete.with_context(leave_skip_state_check=True).unlink()
 
         employee_allocations = self.env['hr.leave.allocation'].search([
             ('employee_id', '=', self.employee_id.id),
@@ -34,15 +51,16 @@ class HrDepartureWizard(models.TransientModel):
         ])
         to_delete = self.env['hr.leave.allocation']
         to_modify = self.env['hr.leave.allocation']
+        allocation_text = _('Validity End date has been updated because \
+            the employee will leave the company on %(departure_date)s.',
+            departure_date=self.departure_date
+        )
         for allocation in employee_allocations:
             if allocation.date_from > self.departure_date:
                 to_delete |= allocation
             else:
                 to_modify |= allocation
-                allocation.message_post(
-                    body=_('Validity End date has been updated because Employee no longer works in the company'),
-                    subtype_xmlid='mail.mt_comment'
-                )
+                allocation.message_post(body=allocation_text, subtype_xmlid='mail.mt_comment')
         to_delete.write({'state': 'confirm'}) # Needs to be confirmed before it can be unlinked
         to_delete.unlink()
         to_modify.date_to = self.departure_date
