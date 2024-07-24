538e2ef3be69c893f7d5cfefc8c294a328a78f86
dhba <dhba@odoo.com>
2024-07-24 14:14:00 +0530

    [IMP] html_editor: ensure muted text appears only on active lines
    Purpose:
    
    Ensure that muted text is displayed exclusively on active
    lines while remaining absent from other lines.
    
    task-3875225

--- addons/html_editor/static/src/main/hint_plugin.js
+++ addons/html_editor/static/src/main/hint_plugin.js
@@ -1,7 +1,8 @@
 import { Plugin } from "@html_editor/plugin";
-import { isEmpty, isProtected, isProtecting } from "@html_editor/utils/dom_info";
+import { isEmptyBlock, isProtected } from "@html_editor/utils/dom_info";
 import { removeClass } from "@html_editor/utils/dom";
-import { selectElements } from "@html_editor/utils/dom_traversal";
+import { selectElements, closestElement } from "@html_editor/utils/dom_traversal";
+import { closestBlock } from "../utils/blocks";
 
 function isMutationRecordSavable(record) {
     return !(record.type === "attributes" && record.attributeName === "placeholder");
@@ -31,11 +32,10 @@ export class HintPlugin extends Plugin {
         const resources = {
             mutation_filtered_classes: ["o-we-hint"],
             is_mutation_record_savable: isMutationRecordSavable,
-            onSelectionChange: p.updateTempHint.bind(p),
-            onExternalHistorySteps: p.updateHints.bind(p),
+            onSelectionChange: p.updateHints.bind(p),
         };
         if (p.config.placeholder) {
-            resources.temp_hints = {
+            resources.hints = {
                 text: p.config.placeholder,
                 target,
             };
@@ -45,7 +45,7 @@ export class HintPlugin extends Plugin {
     };
 
     setup() {
-        this.tempHint = null;
+        this.hint = null;
         this.updateHints(this.editable);
     }
 
@@ -70,36 +70,33 @@ export class HintPlugin extends Plugin {
     /**
      * @param {HTMLElement} [root]
      */
-    updateHints(root = this.editable) {
-        this.clearHints(root);
-        this.makeEmptyBlockHints(root);
-        this.updateTempHint(this.shared.getSelectionData());
-    }
-
-    makeEmptyBlockHints(root) {
-        for (const { selector, hint } of this.resources.emptyBlockHints) {
-            for (const el of selectElements(root, selector)) {
-                // @todo: consider using isEmptyBlock instead.
-                if (isEmpty(el) && !isProtected(el) && !isProtecting(el)) {
-                    this.makeHint(el, hint);
-                }
-            }
+    updateHints() {
+        const selectionData = this.shared.getSelectionData();
+        const editableSelection = selectionData.editableSelection;
+        if (this.hint) {
+            const blockEl = closestBlock(editableSelection.anchorNode);
+            this.removeHint(this.hint);
+            this.removeHint(blockEl);
         }
-    }
-
-    updateTempHint(selectionData) {
-        if (this.tempHint) {
-            this.removeHint(this.tempHint);
-        }
-
-        if (selectionData.editableSelection.isCollapsed) {
-            for (const hint of this.resources["temp_hints"]) {
-                const target = hint.target(selectionData, this.editable);
-                // Do not replace an existing empty block hint by a temp hint.
-                if (target && !target.classList.contains("o-we-hint")) {
-                    this.makeHint(target, hint.text);
-                    this.tempHint = target;
-                    return;
+        if (editableSelection.isCollapsed) {
+            for (const hint of this.resources["hints"]) {
+                if (hint.selector) {
+                    const el = closestElement(editableSelection.anchorNode, hint.selector);
+                    if (el && !isProtected(el) && isEmptyBlock(el)) {
+                        if (this.hint) {
+                            this.removeHint(this.hint);
+                        }
+                        this.makeHint(el, hint.text);
+                        this.hint = el;
+                    }
+                } else {
+                    const target = hint.target(selectionData, this.editable);
+                    // Do not replace an existing empty block hint by a temp hint.
+                    if (target && !target.classList.contains("o-we-hint")) {
+                        this.makeHint(target, hint.text);
+                        this.hint = target;
+                        return;
+                    }
                 }
             }
         }
@@ -113,8 +110,8 @@ export class HintPlugin extends Plugin {
     removeHint(el) {
         el.removeAttribute("placeholder");
         removeClass(el, "o-we-hint");
-        if (this.tempHint === el) {
-            this.tempHint = null;
+        if (this.hint === el) {
+            this.hint = null;
         }
     }
 
