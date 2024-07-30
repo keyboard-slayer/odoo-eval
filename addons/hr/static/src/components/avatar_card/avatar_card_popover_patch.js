c2abc9a13d89e2bd1615b9e51604fb6c0cc49e70
Prakash Prajapati <ppr@odoo.com>
2024-07-30 14:33:38 +0530

    [IMP] employee

--- addons/hr/static/src/components/avatar_card/avatar_card_popover_patch.js
+++ addons/hr/static/src/components/avatar_card/avatar_card_popover_patch.js
@@ -6,20 +6,7 @@ import { AvatarCardPopover } from "@mail/discuss/web/avatar_card/avatar_card_pop
 export const patchAvatarCardPopover = {
     setup() {
         super.setup();
-        this.userInfoTemplate = "hr.avatarCardUserInfos";
-    },
-    get fieldNames() {
-        const fields = super.fieldNames;
-        return fields.concat([
-            "work_phone",
-            "work_email",
-            "work_location_name",
-            "work_location_type",
-            "job_title",
-            "department_id",
-            this.props.recordModel ? "employee_id" : "employee_ids",
         this.userInfoTemplate = "hr.avatarCardEmployeeInfos";
-        this.actionService = useService("action");
     },
     get fieldSpecification() {
         return {
@@ -55,12 +42,6 @@ export const patchAvatarCardPopover = {
             this.employee.id,
         ]);
         this.actionService.doAction(action);
-    },
-    get email() {
-        return this.user.work_email || this.user.email;
-    },
-    get phone() {
-        return this.user.work_phone || this.user.phone;
     },
     async getProfileAction() {
         return this.user.employee_ids?.length > 0
