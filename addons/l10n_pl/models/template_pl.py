04d6ab4e6b70ca9c5d8ffb41bd6939ed20420b17
Thomas Becquevort (thbe) <thbe@odoo.com>
2024-08-13 18:45:18 +0200

    [IMP] l10n_pl: add tag_ids to CoA
    Description of the issue/behavior this commit addresses:
    
    Currently, the Polish CoA doesn't use tag_ids which makes it pretty hard to
    add new accounts.
    
    Desired behavior after this commit is merged:
    
    The Polish CoA uses tag_ids for his accounts and the reports (Balance Sheet
    Micro and Small et Profit & Loss Micro and Small) use those tag_ids to be
    built.
    
    task-3950432

--- addons/l10n_pl/models/template_pl.py
+++ addons/l10n_pl/models/template_pl.py
@@ -13,7 +13,7 @@ class AccountChartTemplate(models.AbstractModel):
             'property_account_payable_id': 'chart21000100',
             'property_account_expense_categ_id': 'chart70010100',
             'property_account_income_categ_id': 'chart73000100',
-            'code_digits': '8',
+            'code_digits': '10',
             'use_storno_accounting': True,
         }
 
@@ -22,9 +22,9 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.pl',
-                'bank_account_code_prefix': '11.000.00',
-                'cash_account_code_prefix': '12.000.00',
-                'transfer_account_code_prefix': '11.090.00',
+                'bank_account_code_prefix': '11.000.',
+                'transfer_account_code_prefix': '11.090.',
+                'cash_account_code_prefix': '12.000.',
                 'account_default_pos_receivable_account_id': 'chart20000200',
                 'income_currency_exchange_account_id': 'chart75000600',
                 'expense_currency_exchange_account_id': 'chart75010400',
@@ -34,3 +34,10 @@ class AccountChartTemplate(models.AbstractModel):
                 'default_cash_difference_expense_account_id': 'chart75010500',
             },
         }
+
+    def _post_load_data(self, template_code, company, template_data):
+        super()._post_load_data(template_code, company, template_data)
+        tags = self.env["account.account.tag"].search([("country_id.code", "=", "PL"), ("name", "in", ["bs_assets_b_3_1_c_1", "small_bs_assets_b_3_a_1", "micro_bs_assets_b_1"])])
+        # Can't include the compnay.country.code in the search because country is a computed field and can't be searched
+        pl_bank_and_cash_accounts = self.env["account.account"].search([("code", "in", ["11.000.001", "11.000.002", "11.000.003", "11.000.004", "11.090.001", "12.000.001"])]).filtered(lambda a: a.company_id.country_id.code == "PL")
+        pl_bank_and_cash_accounts.tag_ids = tags
