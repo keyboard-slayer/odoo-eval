46b0fc148bfb6a968fe60957c5c63b4fb3b32853
vlst-odoo <vlst@odoo.com>
2024-08-28 16:18:00 +0200

    [REF] point_of_sale: remove number_buffer

--- addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -26,6 +26,9 @@ import {
 import { pick } from "@web/core/utils/objects";
 import { unaccent } from "@web/core/utils/strings";
 import { CameraBarcodeScanner } from "@point_of_sale/app/screens/product_screen/camera_barcode_scanner";
+import { makeAwaitable } from "@point_of_sale/app/store/make_awaitable_dialog";
+import { NumberPopup } from "@point_of_sale/app/utils/input_popups/number_popup";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
 export class ProductScreen extends Component {
     static template = "point_of_sale.ProductScreen";
@@ -111,6 +114,121 @@ export class ProductScreen extends Component {
         }));
     }
 
+    async updateSelectedOrderline({ buffer, key }) {
+        const order = this.pos.get_order();
+        const selectedLine = order.get_selected_orderline();
+        // Here we assume that we have a `selectedLine` because the numpad is only visible when
+        // there is a selected line.
+        if (!selectedLine.qty && key === "Backspace") {
+            this._setValue("remove");
+            this.pos.numpadMode = "quantity";
+            return;
+        }
+        // This validation must not be affected by `disallowLineQuantityChange`
+        if (selectedLine.isTipLine() && this.pos.numpadMode !== "price") {
+            this.dialog.add(AlertDialog, {
+                title: _t("Cannot modify a tip"),
+                body: _t("Customer tips, cannot be modified directly"),
+            });
+            return;
+        }
+        if (this.pos.numpadMode === "quantity" && this.pos.disallowLineQuantityChange()) {
+            const orderlines = order.lines;
+            const lastId = orderlines.length !== 0 && orderlines.at(orderlines.length - 1).uuid;
+            const currentQuantity = this.pos.get_order().get_selected_orderline().get_quantity();
+
+            if (selectedLine.noDecrease) {
+                this.dialog.add(AlertDialog, {
+                    title: _t("Invalid action"),
+                    body: _t("You are not allowed to change this quantity"),
+                });
+                return;
+            }
+            const parsedInput = (buffer && parseFloat(buffer)) || 0;
+            if (lastId != selectedLine.uuid) {
+                this._showDecreaseQuantityPopup();
+                return;
+            }
+            if (currentQuantity < parsedInput) {
+                this._setValue(buffer);
+                return;
+            }
+            if (parsedInput < currentQuantity) {
+                this._showDecreaseQuantityPopup();
+                return;
+            }
+            return;
+        }
+        this._setValue(buffer);
+    }
+
+    _setValue(val) {
+        const { numpadMode } = this.pos;
+        let selectedLine = this.currentOrder.get_selected_orderline();
+        if (selectedLine) {
+            if (numpadMode === "quantity") {
+                if (selectedLine.combo_parent_id) {
+                    selectedLine = selectedLine.combo_parent_id;
+                }
+                if (val === "remove") {
+                    this.currentOrder.removeOrderline(selectedLine);
+                } else {
+                    const result = selectedLine.set_quantity(
+                        val,
+                        Boolean(selectedLine.combo_line_ids?.length)
+                    );
+                    for (const line of selectedLine.combo_line_ids) {
+                        line.set_quantity(val, true);
+                    }
+                    if (result !== true) {
+                        this.dialog.add(AlertDialog, result);
+                        this.numberBuffer.reset();
+                    }
+                }
+            } else if (numpadMode === "discount" && val !== "remove") {
+                selectedLine.set_discount(val);
+            } else if (numpadMode === "price" && val !== "remove") {
+                this.setLinePrice(selectedLine, val);
+            }
+        }
+    }
+
+    setLinePrice(line, price) {
+        line.price_type = "manual";
+        line.set_unit_price(price);
+    }
+
+    async _showDecreaseQuantityPopup() {
+        this.numberBuffer.reset();
+        const inputNumber = await makeAwaitable(this.dialog, NumberPopup, {
+            title: _t("Set the new quantity"),
+        });
+        const newQuantity = inputNumber && inputNumber !== "" ? parseFloat(inputNumber) : null;
+        if (newQuantity !== null) {
+            const order = this.pos.get_order();
+            const selectedLine = order.get_selected_orderline();
+            const currentQuantity = selectedLine.get_quantity();
+            if (newQuantity >= currentQuantity) {
+                selectedLine.set_quantity(newQuantity);
+                return true;
+            }
+            if (newQuantity >= selectedLine.saved_quantity) {
+                selectedLine.set_quantity(newQuantity);
+                if (newQuantity == 0) {
+                    selectedLine.delete();
+                }
+                return true;
+            }
+            const newLine = selectedLine.clone();
+            const decreasedQuantity = selectedLine.saved_quantity - newQuantity;
+            newLine.order = order;
+            newLine.set_quantity(-decreasedQuantity, true);
+            selectedLine.set_quantity(selectedLine.saved_quantity);
+            order.add_orderline(newLine);
+            return true;
+        }
+        return false;
+    }
     getNumpadButtons() {
         const colorClassMap = {
             [this.env.services.localization.decimalPoint]: "o_colorlist_item_color_transparent_6",
@@ -142,14 +260,13 @@ export class ProductScreen extends Component {
             `,
         }));
     }
-    onNumpadClick(buttonValue) {
+    onNumpadClick({ buffer, key: buttonValue }) {
         if (["quantity", "discount", "price"].includes(buttonValue)) {
-            this.numberBuffer.capture();
             this.numberBuffer.reset();
             this.pos.numpadMode = buttonValue;
             return;
         }
-        this.numberBuffer.sendKey(buttonValue);
+        this.updateSelectedOrderline({ buffer, key: buttonValue });
     }
     get currentOrder() {
         return this.pos.get_order();
