odoo/odoo

[IMP] payment: warning if pm does not support manual capture
Prior to this commit
- Certain payment methods did not support manual capture, leading to an empty
  traceback and a warning when users attempted payments with these methods.

Post this commit
- Added a new field to the payment method form, visible in the config tab,
  indicating whether the method supports manual capture.
- updated the demo data for PM based on documentations and testing of
  PM
- Payment methods that do not support manual capture will not be shown in the
  payment form if the provider has manual capture on. In debug mode, the
  reason for exclusion will be indicated.

Task-3949151

addons/payment/utils.py
@@ -44,7 +44,7 @@ def check_access_token(access_token, *values):
 
 # Availability report.
 
-def add_to_report(report, records, available=True, reason=''):
+def add_to_report(report, records, available=True, reason='', filter_provider=False):
     """ Add records to the report with the provided values.
 
         Structure of the report:
@@ -71,6 +71,18 @@ def add_to_report(report, records, available=True, reason=''):
     :return: None
     """
     if report is None or not records:  # The report might not be initialized, or no records to add.
+        return
+
+    if filter_provider:
+        for r in records:
+            report['payment_methods'][r] = {
+                'available': available,
+                'reason': reason,
+            }
+            report['payment_methods'][r]['supported_providers'] = [
+                (p[0], p[1])
+                for p in records[r][0]
+            ]
         return
 
     category = 'providers' if records._name == 'payment.provider' else 'payment_methods'

keyboard-slayer/odoo-eval

addons/payment/utils.py
@@ -46,7 +46,7 @@ def check_access_token(access_token, *values):
 
 # Availability report.
 
-def add_to_report(report, records, available=True, reason=''):
+def add_to_report(report, records, available=True, reason='', filter_provider=False):
     """ Add records to the report with the provided values.
 
         Structure of the report:
@@ -73,6 +73,16 @@ def add_to_report(report, records, available=True, reason=''):
     :return: None
     """
     if report is None or not records:  # The report might not be initialized, or no records to add.
+        return
+
+    if filter_provider:
+        for provider in records:
+            for method in records[provider]:
+                report['payment_methods'][method] = {
+                    'available': available,
+                    'reason': reason,
+                }
+                report['payment_methods'][method].setdefault('supported_providers', []).append((provider, False))
         return
 
     category = 'providers' if records._name == 'payment.provider' else 'payment_methods'