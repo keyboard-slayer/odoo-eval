ac70a3c35ca31a0914fce416644e114fe2196508
Xavier Morel <xmo@odoo.com>
2024-09-04 12:36:47 +0200

    [ADD] core: runbot autotag plugin for pytest
    Complements the odoo pytest plugin, by being able to load and convert
    tests tagged (excluded / silenced) on runbot into local skips.
    
    Limitations:
    
    - No version locality / awareness, if a test is disabled because it's
      broken in branch A, it'll also be disabled in branch B even though
      it could be working there.
    - No (http) caching support, as the runbot endpoint does not expose
      any sort of caching information.
    - No CLI or configuration option, the plugin does only one thing,
      enable / disable it to enable / disable the feature.

---
+++ odoo/tests/autotags.py
@@ -0,0 +1,62 @@
+# - fetch https://runbot.odoo.com/runbot/auto-tags (check if modified and shit)
+# - during `pytest_collection_modifyitems`, check items against tags and skip()
+#   them
+# - use can ignore the plugin or `-p no:odoo.tests.autotags` to disable the feature
+import re
+import urllib.error
+import urllib.request
+from typing import Callable
+
+import pytest
+
+
+TagPredicate = pytest.StashKey[Callable[[pytest.Item], None]]()
+tag_re = re.compile(r"""
+    # runbot automatically prepends `-` to disable
+    -
+    (:?/(?P<module>\w+))?
+    (:?:(?P<class>\w+))?
+    (:?\.(?P<method>\w+))?
+""" , re.VERBOSE)
+
+
+def pytest_configure(config: pytest.Config):
+    if config.getoption('--help'):
+        return
+
+    try:
+        r = urllib.request.urlopen("https://runbot.odoo.com/runbot/auto-tags", timeout=1)
+    except urllib.error.URLError:
+        return
+
+    # ignore match failure as technically the runbot *could* use an actual tag(ged)
+    predicates = []
+    for m in filter(None, map(tag_re.fullmatch, r.read().decode().split(','))):
+        pred = []
+        if n := m['method']:
+            pred.append(f'fn.__name__ == {n!r}')
+        if n := m['class']:
+            n += '.'
+            pred.append(f'fn.__qualname__.startswith({n!r})')
+        if n := m['module']:
+            n = f'odoo.addons.{n}.'
+            pred.append(f'fn.__module__.startswith({n!r})')
+
+        if pred:
+            predicates.append(" and ".join(pred))
+
+    if predicates:
+        tagged = eval("lambda fn: " + " or ".join(f"({p})" for p in predicates), {'__builtins__': {}}, {})
+    else:
+        tagged = lambda _: False
+
+    config.stash[TagPredicate] = tagged
+
+
+def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
+    skip_tagged = pytest.mark.skip(reason="tagged on runbot")
+    p = config.stash[TagPredicate]
+    for item in items:
+        # all items here seem to be function but might as well check
+        if isinstance(item, pytest.Function) and p(item.function):
+            item.add_marker(skip_tagged)
