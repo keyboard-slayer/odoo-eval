030e6ccf49be45da6a3fc8efcb1e1222fd99e794
Xavier Morel <xmo@odoo.com>
2024-09-10 16:19:22 +0200

    [IMP] product: refactor some pricelist code
    During pricelist lookup, early exit if all partners have been matched
    with a pricelist during the first step, and for that first step
    directly look up by concrete id if we have a newid, it's not possible
    for `Property` to return values for a newid (since we dereference the
    origins) so doing the first lookup into `specific_properties` with a
    newid is a waste of time, we can just dereference to the origin
    immediately.
    
    When writing on a company, the checks make very little sense and don't
    seem necessary: if we update the currency of a company, it probably
    makes sense to set up a pricelist right away? Also extend and better
    document the test checking that we create a pricelist using the
    *post-write* currency.

--- addons/product/models/product_pricelist.py
+++ addons/product/models/product_pricelist.py
@@ -9,7 +9,7 @@ class Pricelist(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Pricelist"
     _rec_names_search = ['name', 'currency_id']  # TODO check if should be removed
-    _order = "sequence, id, name"
+    _order = "sequence, id"
 
     def _default_currency_id(self):
         return self.env.company.currency_id.id
@@ -295,50 +295,39 @@ class Pricelist(models.Model):
             instead of current user's company
         :return: a dict {partner_id: pricelist}
         """
-        # `partner_ids` might be ID from inactive users. We should use active_test
-        # as we will do a search() later (real case for website public user).
-        Partner = self.env['res.partner'].with_context(active_test=False)
         company_id = self.env.company.id
-
         Property = self.env['ir.property'].with_company(company_id)
-        Pricelist = self.env['product.pricelist']
-        pl_domain = self._get_partner_pricelist_multi_search_domain_hook(company_id)
 
         # if no specific property, try to find a fitting pricelist
         specific_properties = Property._get_multi(
-            'property_product_pricelist', Partner._name,
+            'property_product_pricelist', 'res.partner',
             list(models.origin_ids(partner_ids)),  # Some NewID can be in the partner_ids
         )
         result = {}
         remaining_partner_ids = []
         for pid in partner_ids:
-            if (
-                specific_properties.get(pid)
-                and specific_properties[pid]._get_partner_pricelist_multi_filter_hook()
-            ):
-                result[pid] = specific_properties[pid]
-            elif (
-                isinstance(pid, models.NewId) and specific_properties.get(pid.origin)
-                and specific_properties[pid.origin]._get_partner_pricelist_multi_filter_hook()
-            ):
-                result[pid] = specific_properties[pid.origin]
+            rid = pid or pid.origin
+            if (p := specific_properties.get(rid)) and p._get_partner_pricelist_multi_filter_hook():
+                result[pid] = specific_properties[rid]
             else:
                 remaining_partner_ids.append(pid)
 
-        if remaining_partner_ids:
-            # get fallback pricelist when no pricelist for a given country
-            pl_fallback = (
-                Pricelist.search(pl_domain + [('country_group_ids', '=', False)], limit=1) or
-                Property._get('property_product_pricelist', 'res.partner') or
-                Pricelist.search(pl_domain, limit=1)
-            )
-            # group partners by country, and find a pricelist for each country
-            remaining_partners = self.env['res.partner'].browse(remaining_partner_ids)
-            partners_by_country = remaining_partners.grouped('country_id')
-            for country, partners in partners_by_country.items():
-                pl = Pricelist.search(pl_domain + [('country_group_ids.country_ids', '=', country.id if country else False)], limit=1)
-                pl = pl or pl_fallback
-                result.update(dict.fromkeys(partners._ids, pl))
+        if not remaining_partner_ids:
+            return result
+
+        Pricelist = self.env['product.pricelist']
+        pl_domain = self._get_partner_pricelist_multi_search_domain_hook(company_id)
+        # get fallback pricelist when no pricelist for a given country
+        pl_fallback = (
+            Pricelist.search(pl_domain + [('country_group_ids', '=', False)], limit=1) or
+            Property._get('property_product_pricelist', 'res.partner') or
+            Pricelist.search(pl_domain, limit=1)
+        )
+        # group partners by country, and find a pricelist for each country
+        partners_by_country = self.env['res.partner'].browse(remaining_partner_ids).grouped('country_id')
+        for country, partners in partners_by_country.items():
+            pl = Pricelist.search(pl_domain + [('country_group_ids.country_ids', '=', country.id if country else False)], limit=1)
+            result.update(dict.fromkeys(partners._ids, pl or pl_fallback))
 
         return result
 
