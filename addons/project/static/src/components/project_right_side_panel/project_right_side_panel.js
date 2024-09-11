odoo/odoo

[IMP] project, sale_timesheet: enhance milestone management in rightside panel
This commit focuses on improving the functionality of milestones in the
right-side panel of the project. The goal is to provide a more
convenient and efficient way of managing milestones.

task-3186710

addons/project/static/src/components/project_right_side_panel/project_right_side_panel.js
@@ -4,7 +4,6 @@ import { _t } from "@web/core/l10n/translation";
 import { useService } from '@web/core/utils/hooks';
 import { formatFloat } from "@web/views/fields/formatters";
 import { ViewButton } from '@web/views/view_button/view_button';
-import { FormViewDialog } from '@web/views/view_dialogs/form_view_dialog';
 
 import { ProjectRightSidePanelSection } from './components/project_right_side_panel_section';
 import { ProjectMilestone } from './components/project_milestone';
@@ -109,34 +108,22 @@ export class ProjectRightSidePanel extends Component {
         return data;
     }
 
-    async loadMilestones() {
-        const milestones = await this.orm.call(
-            'project.project',
-            'get_milestones',
-            [[this.projectId]],
-            { context: this.context },
-        );
-        this.state.data.milestones = milestones;
-        return milestones;
-    }
-
-    addMilestone() {
-        const context = {
-            ...this.context,
-            'default_project_id': this.projectId,
-        };
-        this.openFormViewDialog({
-            context,
-            title: _t('New Milestone'),
-            resModel: 'project.milestone',
-            onRecordSaved: async () => {
-                await this.loadMilestones();
-            },
+    async viewTasks() {
+        this.actionService.doActionButton({
+            type: "object",
+            resId: this.projectId,
+            name: "action_view_tasks_from_project_milestone",
+            resModel: "project.project",
         });
     }
 
-    async openFormViewDialog(params, options = {}) {
-        this.dialog.add(FormViewDialog, params, options);
+    async addMilestone() {
+        this.actionService.doActionButton({
+            type: "object",
+            resId: this.projectId,
+            name: "action_get_list_view_project_update",
+            resModel: "project.project",
+        });
     }
 
     async onProjectActionClick(params) {

keyboard-slayer/odoo-eval

addons/project/static/src/components/project_right_side_panel/project_right_side_panel.js
@@ -2,7 +2,6 @@ import { _t } from "@web/core/l10n/translation";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { formatFloat } from "@web/views/fields/formatters";
 import { ViewButton } from '@web/views/view_button/view_button';
-import { FormViewDialog } from '@web/views/view_dialogs/form_view_dialog';
 
 import { ProjectRightSidePanelSection } from './components/project_right_side_panel_section';
 import { ProjectMilestone } from './components/project_milestone';
@@ -132,34 +131,22 @@ export class ProjectRightSidePanel extends Component {
         return data;
     }
 
-    async loadMilestones() {
-        const milestones = await this.orm.call(
-            'project.project',
-            'get_milestones',
-            [[this.projectId]],
-            { context: this.context },
-        );
-        this.state.data.milestones = milestones;
-        return milestones;
-    }
-
-    addMilestone() {
-        const context = {
-            ...this.context,
-            'default_project_id': this.projectId,
-        };
-        this.openFormViewDialog({
-            context,
-            title: _t('New Milestone'),
-            resModel: 'project.milestone',
-            onRecordSaved: async () => {
-                await this.loadMilestones();
-            },
+    async viewTasks() {
+        this.actionService.doActionButton({
+            type: "object",
+            resId: this.projectId,
+            name: "action_view_tasks_from_project_milestone",
+            resModel: "project.project",
         });
     }
 
-    async openFormViewDialog(params, options = {}) {
-        this.dialog.add(FormViewDialog, params, options);
+    async viewMilestones() {
+        this.actionService.doActionButton({
+            type: "object",
+            resId: this.projectId,
+            name: "action_get_list_view",
+            resModel: "project.project",
+        });
     }
 
     async onProjectActionClick(params) {