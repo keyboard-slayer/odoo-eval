odoo/odoo

[IMP] hr, {resource_}mail: open partners avatar card on click of @mentions
Prior to this commit:
Clicking on a partner's @mention in the chat interface would directly open
the corresponding chat thread.

Following this commit:
Upon clicking a partner's @mention, the system now presents an avatar card
containing pertinent information about the mentioned partner. Additionally,
if the partner has a userId then display the 'Send Message'.

task-3816952

addons/mail/static/src/discuss/web/avatar_card/avatar_card_popover.js
@@ -1,13 +1,19 @@
 import { useService } from "@web/core/utils/hooks";
 import { Component, onWillStart } from "@odoo/owl";
 import { useOpenChat } from "@mail/core/web/open_chat_hook";
+import { rpc } from "@web/core/network/rpc";
 
 export class AvatarCardPopover extends Component {
     static template = "mail.AvatarCardPopover";
 
     static props = {
         id: { type: Number, required: true },
+        resModel: { type: String, optional: true },
         close: { type: Function, required: true },
+    };
+
+    static defaultProps = {
+        resModel: "res.users",
     };
 
     setup() {
@@ -15,12 +21,19 @@ export class AvatarCardPopover extends Component {
         this.orm = useService("orm");
         this.openChat = useOpenChat("res.users");
         onWillStart(async () => {
-            [this.user] = await this.orm.read("res.users", [this.props.id], this.fieldNames);
+            [this.user] = await rpc("/mail/avatar_card/info", {
+                avatar_id: this.props.id,
+                resModel: this.props.resModel,
+                fieldNames: this.fieldNames,
+            });
         });
     }
 
     get fieldNames() {
-        return ["name", "email", "phone", "im_status", "share", "partner_id"];
+        return {
+            partnerwithuser: ["name", "email", "phone", "im_status", "share", "partner_id"],
+            partnerwithoutuser: ["name", "phone", "email", "im_status", "partner_share"],
+        };
     }
 
     get email() {
@@ -31,13 +44,22 @@ export class AvatarCardPopover extends Component {
         return this.user.phone;
     }
 
+    get userShare() {
+        if (this.props.resModel === "res.users") {
+            return this.user.share;
+        } else {
+            return this.user.partner_share;
+        }
+    }
+
     get showViewProfileBtn() {
         return true;
     }
 
     async getProfileAction() {
+        const id = this.user.partner_id?.[0] ?? this.user.id;
         return {
-            res_id: this.user.partner_id[0],
+            res_id: id,
             res_model: "res.partner",
             type: "ir.actions.act_window",
             views: [[false, "form"]],

addons/resource_mail/static/src/components/avatar_card_resource/avatar_card_resource_popover.js
@@ -29,7 +29,11 @@ export class AvatarCardResourcePopover extends AvatarCardPopover {
     }
 
     async onWillStart() {
-        [this.record] = await this.orm.read(this.props.recordModel, [this.props.id], this.fieldNames);
+        [this.record] = await this.orm.read(
+            this.props.recordModel,
+            [this.props.id],
+            this.fieldNames
+        );
         await Promise.all(this.loadAdditionalData());
     }
 
@@ -40,7 +44,8 @@ export class AvatarCardResourcePopover extends AvatarCardPopover {
 
     get fieldNames() {
         const excludedFields = new Set(["partner_id"]);
-        return super.fieldNames
+        const fields = super.fieldNames;
+        return fields["partnerwithuser"]
             .concat(["user_id", "resource_type"])
             .filter((field) => !excludedFields.has(field));
     }

keyboard-slayer/odoo-eval

addons/mail/static/src/discuss/web/avatar_card/avatar_card_popover.js
@@ -7,7 +7,12 @@ export class AvatarCardPopover extends Component {
 
     static props = {
         id: { type: Number, required: true },
+        resModel: { type: String, optional: true },
         close: { type: Function, required: true },
+    };
+
+    static defaultProps = {
+        resModel: "res.users",
     };
 
     setup() {
@@ -15,20 +20,36 @@ export class AvatarCardPopover extends Component {
         this.orm = useService("orm");
         this.openChat = useOpenChat("res.users");
         onWillStart(async () => {
-            [this.user] = await this.orm.read("res.users", [this.props.id], this.fieldNames);
+            const modelName = this.props.resModel === "res.users" ? "res.users" : "res.partner";
+            [this.avatarEntity] = await this.orm.read(
+                modelName,
+                [this.props.id],
+                this.fieldNames[this.props.resModel === "res.users" ? "users" : "partners"]
+            );
         });
     }
 
     get fieldNames() {
-        return ["name", "email", "phone", "im_status", "share", "partner_id"];
+        return {
+            users: ["name", "email", "phone", "im_status", "share", "partner_id"],
+            partners: ["name", "email", "phone", "im_status", "partner_share"],
+        };
     }
 
     get email() {
-        return this.user.email;
+        return this.avatarEntity.email;
     }
 
     get phone() {
-        return this.user.phone;
+        return this.avatarEntity.phone;
+    }
+
+    get userShare() {
+        if (this.props.resModel === "res.users") {
+            return this.avatarEntity.share;
+        } else {
+            return this.avatarEntity.partner_share;
+        }
     }
 
     get showViewProfileBtn() {
@@ -36,8 +57,13 @@ export class AvatarCardPopover extends Component {
     }
 
     async getProfileAction() {
+        const id =
+            this.props.resModel === "res.partner"
+                ? this.avatarEntity.id
+                : this.avatarEntity.partner_id[0];
+
         return {
-            res_id: this.user.partner_id[0],
+            res_id: id,
             res_model: "res.partner",
             type: "ir.actions.act_window",
             views: [[false, "form"]],
@@ -45,7 +71,7 @@ export class AvatarCardPopover extends Component {
     }
 
     get userId() {
-        return this.user.id;
+        return this.avatarEntity.id;
     }
 
     onSendClick() {

addons/resource_mail/static/src/components/avatar_card_resource/avatar_card_resource_popover.js
@@ -29,7 +29,11 @@ export class AvatarCardResourcePopover extends AvatarCardPopover {
     }
 
     async onWillStart() {
-        [this.record] = await this.orm.read(this.props.recordModel, [this.props.id], this.fieldNames);
+        [this.record] = await this.orm.read(
+            this.props.recordModel,
+            [this.props.id],
+            this.fieldNames
+        );
         await Promise.all(this.loadAdditionalData());
     }
 
@@ -40,7 +44,8 @@ export class AvatarCardResourcePopover extends AvatarCardPopover {
 
     get fieldNames() {
         const excludedFields = new Set(["partner_id"]);
-        return super.fieldNames
+        const fields = super.fieldNames;
+        return fields["users"]
             .concat(["user_id", "resource_type"])
             .filter((field) => !excludedFields.has(field));
     }