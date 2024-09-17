5724d5a76176ad90ef5c138470c407ca17dceb33
Paul Stroobant <stpa@odoo.com>
2024-09-17 08:10:20 +0200

    [FIX] l10n_mx: restrict account codes to coa sat
    Steps to reproduce the issue:
    
    1. Activate the Odoo Mexican Localization Reports module
    2. In a Mexican company, create a new Account with 1 as code
    3. Go to Trial Balance and download COA SAT (XML)
    5. In the General Settings with developer mode active, Download XSD files
    6. Go to Trial Balance and download COA SAT (XML) again
    7. You get a UserError with an unclear message
    
    Explanation:
    
    The Mexican Chart of Accounts have clear rules regarding `account.account.code`.
    The only way to verify those accounts is through the XSD files check, but they are not automatically downloaded and the error received with those files downloaded is not user friendly.
    
    Fix reasoning:
    
    Instead of regulating the code when downloading the XML, we will do so on creation or update of `account.account`.
    The SAT codes are retrieved from a local file to avoid fetch requests on every module update or installation.
    
    opw-4103681

--- addons/l10n_mx/models/account_account.py
+++ addons/l10n_mx/models/account_account.py
@@ -1,4 +1,11 @@
-from odoo import api, Command, models
+import re
+from odoo import _, api, Command, models
+from odoo.exceptions import RedirectWarning
+from odoo.tools import file_open
+
+PREFIX_COLUMN_VALUES = [line.split(',')[-1] for line in file_open('l10n_mx/data/account.group.template.csv').read().split('\n')]  # Getting last column value from each row
+SAT_CODES = [word[1:7] for word in PREFIX_COLUMN_VALUES if len(word) == 8]  # Filtering only "XXX.YY" values and removing quotes from inside the string
+THIRD_SECTION = re.compile(r'\.\d{1,2}[1-9]')  # Must start with a '.', have two or three digits and the last one cannot be 0
 
 
 class AccountAccount(models.Model):
@@ -17,3 +24,20 @@ class AccountAccount(models.Model):
             tag_id = debit_tag.id if account.code[0] in DEBIT_CODES else credit_tag.id
             account.tag_ids = [Command.link(tag_id)]
         return accounts
+
+    @api.constrains('code', 'group_id')
+    def ensure_code_conforms_coa_sat(self):
+        incorrect_accounts = self.filtered(
+                lambda a: a.group_id and a.account_type != 'equity_unaffected' and a.company_id.country_code == 'MX'
+                and (a.code[:6] not in SAT_CODES or not THIRD_SECTION.fullmatch(a.code[6:]))
+        )
+        if incorrect_accounts:
+            account_names = ''.join(f'\n -   {a.name}' for a in incorrect_accounts)
+            raise RedirectWarning(
+                _("Some of your accounts do not respect the SAT code regulation.\n%s", account_names),
+                {
+                    'type': 'ir.actions.act_url',
+                    'url': 'https://www.odoo.com/documentation/16.0/applications/finance/fiscal_localizations/mexico.html#chart-of-accounts'
+                },
+                _("Check the documentation")
+            )
