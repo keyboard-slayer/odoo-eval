e54b11afb007f437247f8196c11e176f1ab4d3d4
Adnan Saiyed <adsa@odoo.com>
2024-09-18 15:28:22 +0530

    [FIX] web_editor: selection fails on link when applying style
    Behaviour before commit:
    
    In website, when trying to apply color or any style on a link
    from toolbar whole link gets selected. This happens because
    in wysiwyg.js destroyLinkTools method gets called when user
    clicks on toolbar to apply color.
    
    Behaviour after commit:
    
    Now, any style can be applied on a link without selecting
    whole link.
    
    task-4072867

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1161,9 +1161,12 @@ const Wysiwyg = Widget.extend({
                 }
                 this.linkTools.noFocusUrl = options.noFocusUrl;
                 const _onClick = ev => {
+                    const selection = this.odooEditor.document.getSelection();
+                    const isFocusOnInput = closestElement(selection.anchorNode, '.o_url_input');
                     if (
+                        (isFocusOnInput && ev.target.closest('.oe-toolbar')) ||
                         !ev.target.closest('#create-link') &&
-                        (!ev.target.closest('.oe-toolbar') || !ev.target.closest('we-customizeblock-option')) &&
+                        (!ev.target.closest('.oe-toolbar') && !ev.target.closest('we-customizeblock-option')) &&
                         !ev.target.closest('.ui-autocomplete') &&
                         (!this.linkTools || ![ev.target, ...wysiwygUtils.ancestors(ev.target)].includes(this.linkTools.$link[0]))
                     ) {
